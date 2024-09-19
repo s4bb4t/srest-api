e98f729ae8fe6b4f046a399f8fbc931972d4dd92
s4bb4t <mentalrapewhr@gmail.com>
2024-09-19 14:29:22 +0300

    bug fixes

--- internal/database/database.go
+++ internal/database/database.go
@@ -23,7 +23,7 @@ func SetupDataBase(dbStr string) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %v", op, err)
 	}
 
-	migrationsDir := "srest-api/internal/database/migrations"
+	migrationsDir := "../../internal/database/migrations"
 
 	if err := runMigrations(db, migrationsDir); err != nil {
 		return nil, fmt.Errorf("%s: %v", op, err)
