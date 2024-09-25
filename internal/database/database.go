6aabf3d2f04e689dfd7b2c35489c350e0a6593bf
s4bb4t <mentalrapewhr@gmail.com>
2024-09-25 13:07:42 +0300

    for tests

--- internal/database/database.go
+++ internal/database/database.go
@@ -23,7 +23,7 @@ func SetupDataBase(dbStr, env string) (*Storage, error) {
 		return nil, fmt.Errorf("%s: %v", op, err)
 	}
 
-	if env != "prod" {
+	if env == "local" {
 		migrationsDir := "./internal/database/migrations"
 		fmt.Println("Migrations directory:", migrationsDir) // Проверить путь
 
