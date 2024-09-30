2a51b7bc63c7af2a5399e4559977026003739f40
s4bb4t <mentalrapewhr@gmail.com>
2024-09-30 13:03:50 +0300

    docs fixes

--- cmd/sapi/main.go
+++ cmd/sapi/main.go
@@ -31,9 +31,9 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      51.250.113.72:8082
+// @host 51.250.113.72:8082
 
-// @BasePath  /api/v1
+// @BasePath /api/v1
 
 // @securityDefinitions.apikey BearerAuth
 // @in header
