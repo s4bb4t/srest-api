30fe0c863cc68e844d3d5e688396e29e72409698
s4bb4t <mentalrapewhr@gmail.com>
2024-09-30 12:15:50 +0300

    fixes

--- cmd/sapi/main.go
+++ cmd/sapi/main.go
@@ -31,7 +31,7 @@ import (
 // @license.name  Apache 2.0
 // @license.url   http://www.apache.org/licenses/LICENSE-2.0.html
 
-// @host      easydev.club
+// @host      51.250.113.72:8082
 
 // @BasePath  /api/v1
 
